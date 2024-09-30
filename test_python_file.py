89fd3302f206f71372cca2a80f682e85fba07971
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2024-09-30 13:55:14 -0700

    At Engin's suggestion, put visualization step into test_python_file.py
    He pointed this out when I demo'd it to the team today
    
    ----
    Signed-off-by: Lydia Duncan <lydia-duncan@users.noreply.github.com>

--- test_python_file.py
+++ test_python_file.py
@@ -1,10 +1,19 @@
 import betaDiversity
 
+import xarray as xr
+import netCDF4
+import matplotlib.pyplot as plt
+
+# set up Chapel library
 betaDiversity.chpl_setup()
 
+# run Chapel library
 result = betaDiversity.beta_diversity(in_name="Utila", map_type="geomorphic",
                                       window_size=12732)
 
-print(result)
+# visually display the result
+file = xr.open_dataset(result)
+file['beta_diversity'].plot()
+plt.show()
 
 betaDiversity.chpl_cleanup()
