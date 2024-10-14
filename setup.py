eecf5a1f8e8e4b8f00aa726258db71645f86e8d8
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2024-10-14 11:19:06 -0700

    Update setup.py to install Chapel
    Clone the repo and use `configure` to set it up in a way that will be accessible for later subprocess runs

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ import subprocess
 from setuptools import setup
 
 os.environ["CHPL_LIB_PIC"] = "pic"
-# TODO: install Chapel
-# install netcdf?
+
+subprocess.run("git", "clone", "https://github.com/chapel-lang/chapel.git")
+os.chdir("chapel")
+subprocess.run("./configure")
+subprocess.run("make")
+subprocess.run("make install")
+
+os.chdir("..")
 
 subprocess.run("chpl betaDiversity.chpl -lnetcdf --fast --library --library-python --library-dir=chapel-beta-diversity", shell=True)
 
