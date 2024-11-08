390750584392873357a288dcd225024972c46a72
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2024-11-08 15:17:04 -0800

    Try removing the delete to see if it helps
    This one I might be more okay with leaving in, but it'd still be good to figure out *why* we have trouble with cleanup

--- test_python_file.py
+++ test_python_file.py
@@ -6,5 +6,3 @@ result = betaDiversity.beta_diversity(in_name="Utila", map_type="geomorphic",
                                       window_size=12732)
 
 print(result)
-
-betaDiversity.chpl_cleanup()
