4439691260bef6e4ffe00d221c0ef4a5d346f5fe
Lydia Duncan <lydia-duncan@users.noreply.github.com>
2024-11-12 11:05:08 -0800

    Restore cleanup call while I check to see if a fix I'm making helps with it

--- test_python_file.py
+++ test_python_file.py
@@ -6,3 +6,5 @@ result = betaDiversity.beta_diversity(in_name="Utila", map_type="geomorphic",
                                       window_size=12732)
 
 print(result)
+
+betaDiversity.chpl_cleanup()
